df20e6d6782d94fb4bcbc89e55c6481b318dc15b
g-k-m <george.mechkov123@gmail.com>
2020-12-17 15:51:54 +0200

    updated server

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,49 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './Components/App/app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { AppHttpInterceptor } from './Core/Services/Interceptors/app-http.interceptor';
-import { HeaderComponent } from './Components/Shared/header/header.component';
-import { HomeComponent } from './Components/home/home.component';
-import { LoginPageComponent } from './Components/Authentication/login-page/login-page.component';
-import { RegisterPageComponent } from './Components/Authentication/register-page/register-page.component';
-import { EditProductComponent } from './Components/edit-product/edit-product.component';
-import { CreateProductComponent } from './Components/create-product/create-product.component';
-import { AllOrdersComponent } from './Components/all-orders/all-orders.component';
-import { MyOrdersComponent } from './Components/my-orders/my-orders.component';
-import { ProductDetailsComponent } from './Components/product-details/product-details.component';
-import { CartComponent } from './Components/cart/cart.component';
-import { OrderDetailsComponent } from './Components/order-details/order-details.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    LoginPageComponent,
-    RegisterPageComponent,
-    EditProductComponent,
-    CreateProductComponent,
-    AllOrdersComponent,
-    MyOrdersComponent,
-    ProductDetailsComponent,
-    CartComponent,
-    OrderDetailsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
+    AppRoutingModule
   ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AppHttpInterceptor,
-    multi: true
-  }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
